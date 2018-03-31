7a60de14accf18ef4f425fed02de2cea4211c0fd
Abhishek Mohanty <abhishek.mohanty.3979@gmail.com>
2018-03-31 21:13:45 +0530

    Add files via upload

---
+++ program-iris.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+regressor=LinearRegression()
+dataframe=pd.read_csv('iris.data')
+labelencoder_y=LabelEncoder()
+x=dataframe.iloc[:,:-1].values 
+y=dataframe.iloc[:,-1].values
+y[:]=labelencoder_y.fit_transform(y[:])
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.0,random_state=0)
+regressor.fit(x_train,y_train)
+x_test=[[6.1,2.9,4.7,1.4],[4.4,2.9,1.4,0.2]]
+y_predict=regressor.predict(x_test)
+y_predict
